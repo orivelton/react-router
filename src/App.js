31e17cbea1d505504d504eb5320fdd07feefbcc5
orivelton <oconceicao@ir7.com.br>
2018-10-09 15:35:53 -0300

    chore: Add router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Link } from 'react-router-dom';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
@@ -9,11 +9,34 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
           </header>
-          <Link to='/sobre'>Sobre</Link>
+          <nav>
+            <ul>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li>
+                <Link to='/sobre'>Sobre</Link>
+              </li>
+              <li>
+                <Link to='/contato'>Contato</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Route path='/sobre' component={Sobre} />
+          <Route path='/contato' component={Contato} />
         </div>
       </BrowserRouter>
     );
   }
 }
+
+const Sobre = () => (
+  <h1>Sobre</h1>
+)
+
+const Contato = () => (
+  <h1>Contato</h1>
+)
 
 export default App;
